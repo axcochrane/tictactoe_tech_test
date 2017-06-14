6cb25671442060409631cacfeb298d34a6897778
Sandy Cochrane <a.x.cochrane@gmail.com>
2017-06-14 16:34:46 +0100

    added print board method

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,7 @@
 class Board
 
   attr_reader :playing_area
-  
+
   def initialize
     @playing_area = [
             [[],[],[]],
--- lib/game.rb
+++ lib/game.rb
@@ -3,7 +3,7 @@ require_relative './board.rb'
 
 class Game
 
-  attr_reader :player_1, :player_2, :players, :active_player, :board
+  attr_reader :players, :active_player, :board, :print_board
 
   def initialize(players = [Player.new, Player.new])
     @players = players
@@ -16,9 +16,17 @@ class Game
     if @board[row][col] != []
       raise "square is taken already!" 
     end
-    @board[row, col] = @active_marker 
+    @board[row][col] = @active_marker 
     @active_player = @players[0] ? @active_player = @players[1] : @active_player = @players[0] 
     @active_marker = 'x' ? @active_marker = 'o' : @active_marker = 'x' 
+  end
+
+  def print_board
+    puts "#{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}"
+    puts "____________"
+    puts "#{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}"
+    puts "____________"
+    puts "#{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}"
   end
 
 end
